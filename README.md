3336ea9fbef3e2b9f808967e35b16c69fa0ac790
range3 <white.line.oar@gmail.com>
2024-03-04 09:28:12 +0000

    Impl translatable --extract

--- README.md
+++ README.md
@@ -3,7 +3,8 @@
 pip install layoutparser torchvision
 pip install wheel numpy cython
 sudo apt install python3-dev
+pip install pillow==9.5.0
 pip install --no-build-isolation 'git+https://github.com/facebookresearch/detectron2.git@v0.6#egg=detectron2'
 pip install "layoutparser[layoutmodels]"
-sudo apt install libgl1-mesa-dev
+sudo apt install libgl1-mesa-dev libglib2.0-0 poppler-utils
 ```
--- translatable/__main__.py
+++ translatable/__main__.py
@@ -1,11 +1,66 @@
 import argparse
 import sys
-import cv2
 import layoutparser as lp
+import numpy as np
+import pdf2image
 
-def extract_text(input_pdf, output_txt):
-    pdf_pages, _ = lp.load_pdf(input_pdf, load_images=True, dpi=72)
-    pass
+# constants
+DPI = 72
+
+
+def is_inside(paragraph_block, text_block):
+    # Set allowable_error_pixel based on paragraph_block's width
+    allowable_error_pixel = 10 if paragraph_block.width > 300 else 3
+
+    # Check if text_block is inside paragraph_block with the allowable error margin
+    return (
+        text_block.block.x_1 >= paragraph_block.block.x_1 - allowable_error_pixel
+        and text_block.block.y_1 >= paragraph_block.block.y_1 - allowable_error_pixel
+        and text_block.block.x_2 <= paragraph_block.block.x_2 + allowable_error_pixel
+        and text_block.block.y_2 <= paragraph_block.block.y_2 + allowable_error_pixel
+    )
+
+
+def text_blocks_to_words(text_blocks, coalesce_threshold=1):
+    if len(text_blocks) == 0:
+        return []
+    words = [text_blocks[0].text]
+    prev_x_2 = text_blocks[0].block.x_2
+    for block in text_blocks[1:]:
+        gap_x = block.block.x_1 - prev_x_2
+        if gap_x > coalesce_threshold:
+            words.append(block.text)
+        else:
+            words[-1] += block.text
+        prev_x_2 = block.block.x_2
+    return words
+
+
+def extract_text(input_pdf):
+    pdf_pages, _ = lp.load_pdf(input_pdf, load_images=True, dpi=DPI)
+    model = lp.Detectron2LayoutModel(
+        "lp://PubLayNet/mask_rcnn_X_101_32x8d_FPN_3x/config",
+        extra_config=["MODEL.ROI_HEADS.SCORE_THRESH_TEST", 0.5],
+        label_map={0: "Text", 1: "Title", 2: "List", 3: "Table", 4: "Figure"},
+    )
+    pdf_images = np.asarray(pdf2image.convert_from_path(input_pdf, dpi=DPI))
+
+    for page_idx, pdf_page in enumerate(pdf_pages):
+        text_blocks = pdf_page.get_homogeneous_blocks()
+        pdf_layout = model.detect(pdf_images[page_idx])
+        paragraph_blocks = [b for b in pdf_layout if b.type == "Text"]
+        paragraph_blocks = sorted(
+            paragraph_blocks, key=lambda x: (x.block.x_1 // 100, x.block.y_1 // 10)
+        )
+
+        for paragraph_block in paragraph_blocks:
+            inner_text_blocks = list(
+                filter(lambda x: is_inside(paragraph_block, x), text_blocks)
+            )
+            words_in_paragraph = text_blocks_to_words(inner_text_blocks)
+            if len(words_in_paragraph) > 10:
+                print(" ".join(words_in_paragraph))
+                print("-" * 30)
 
 
 def replace_text(input_pdf, input_txt, output_pdf):
@@ -29,10 +84,7 @@ def main():
     args = parser.parse_args()
 
     if args.extract:
-        # if sys.stdout.isatty():
-        #     print("Error: Please redirect output to a file.", file=sys.stderr)
-        #     sys.exit(1)
-        extract_text(args.extract, sys.stdout)
+        extract_text(args.extract)
 
     elif args.replace and len(args.replace) == 2:
         input_pdf, input_txt = args.replace
